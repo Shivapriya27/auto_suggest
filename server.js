097a9b7346773467bd250862e3455b2feb69d7eb
sheri shivapriya <sherishivapriya2006@gmail.com>
2026-07-03 11:52:56 +0530

    added express project

---
+++ server.js
@@ -0,0 +1,8 @@
+const express  = require('express');
+const app = express();
+
+const PORT  =  process.env.PORT || 3002;
+
+app.listen(PORT, function(){
+    console.log("Successfully Running at http://localhost:" + PORT);
+})
