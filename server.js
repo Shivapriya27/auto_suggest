671648168fc47ec3c231f7a2464f9733d2b70e3a
sheri shivapriya <sherishivapriya2006@gmail.com>
2026-07-03 12:33:04 +0530

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express  = require('express');
 const app = express();
 
 const PORT  =  process.env.PORT || 3002;
+//web server:
+app.use(express.static("frontend"));
 
 app.listen(PORT, function(){
     console.log("Successfully Running at http://localhost:" + PORT);
